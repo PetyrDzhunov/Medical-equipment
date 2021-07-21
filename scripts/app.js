debc7196b4c7d71af4b369ae327041a52125c6c0
Petyr Dzhunov <petar.djunov@abv.bg>
2021-07-21 22:37:31 +0300

    feat/added-nav-bar

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,23 @@
 //database?
 
 const app = Sammy('#root', function() {
+    this.use('Handlebars', 'hbs');
 
+    this.get('/home', function(context) {
+        console.log('home now');
+        this.loadPartials({
+                'header': '/partials/header.hbs'
+            })
+            .then(function() {
+                this.partial('/templates/home.hbs')
+            })
+    })
 });
 
 
 
+
 app.run('/home')
+
+
+//helper function
