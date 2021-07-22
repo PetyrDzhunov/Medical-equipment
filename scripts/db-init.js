677a21eb50575c31ce26ceb6b52c36d19e64fe61
Petyr Dzhunov <petar.djunov@abv.bg>
2021-07-22 09:45:52 +0300

    added-register-form&route and initialized my database with firebase

---
+++ scripts/db-init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD8zyo5rQf9XmeAbKETE1umdHok4iqkIAM",
+    authDomain: "medical-equipment-e6745.firebaseapp.com",
+    projectId: "medical-equipment-e6745",
+    storageBucket: "medical-equipment-e6745.appspot.com",
+    messagingSenderId: "348317601986",
+    appId: "1:348317601986:web:f2def1a188f5d516e7eb5b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
